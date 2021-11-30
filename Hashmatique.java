f795ed0e0d47100c626bb47b0a43e73d2a8a562a
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-29 23:48:34 -0800

    implement hashmap

--- Hashmatique.java
+++ Hashmatique.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Set;
 
 //  ///////////////////////////////////////////////////////////////
 //  HASHMATIQUE APP
@@ -17,12 +18,19 @@ public class Hashmatique {
         trackList.put("Row, row, row, your boat", "Row, row, row your boat\nGently down the stream\nMerrily merrily, merrily, merrily\nLife is but a dream");
         trackList.put("Twinkle Twinkle", "Twinkle, twinkle, little star\nHow I wonder what you are\nUp above the world so high\nLike a diamond in the sky\nTwinkle, twinkle little star\nHow I wonder what you are");
         trackList.put("Itsy Bitsy Spider", "The itsy bitsy spider crawled up the water spout.\nDown came the rain, and washed the spider out.\nOut came the sun, and dried up all the rain,\nand the itsy bitsy spider went up the spout again");
-        trackList.put("I'm a little tea pot", "I'm a little teapot,\nShort and stout,\nHere is my handle\nHere is my spout\nWhen I get all steamed up,\nHear me shout,\nTip me over and pour me out!");
+        trackList.put("I'm a little teapot", "I'm a little teapot,\nShort and stout,\nHere is my handle\nHere is my spout\nWhen I get all steamed up,\nHear me shout,\nTip me over and pour me out!");
 
         System.out.println("*****************************************************");
-        System.out.println("The Lyrics of Twinkle Twinkle are: " + trackList.get("Twinkle Twinkle"));
+        System.out.println("The Lyrics of Twinkle Twinkle are:\n" + trackList.get("Twinkle Twinkle"));
         System.out.println("");
 
-        
+        System.out.println("*****************************************************");
+        System.out.println("Pull out all the track names and lyrics in the formate Track: Lyrics\n");
+        Set<String> keys = trackList.keySet();
+        for (String key : keys) {
+            System.out.printf("---- Track: %s --------\n", key);
+            System.out.println(trackList.get(key));
+            System.out.println("");
+        }
     }
 }
